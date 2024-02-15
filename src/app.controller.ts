d29b3e267f2c94f65d085288c6f078329125e868
Abhishek Kumar <abhishek.hl.kumar@accenture.com>
2024-02-15 18:21:19 +0530

    api-implementation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface DataItem {
+  id: number;
+  text: string;
+  value: number;
+}
 @Controller('data')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): any {
+  getHello(): DataItem[] {
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,20 +1,43 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+interface DataItem {
+  id: number;
+  text: string;
+  value: number;
+}
 
 @Injectable()
+// export class AppService {
+//   getHello(): any {
+//     return [
+//       { "id": 1, "text": "ALL", "value": 9 },
+//       { "id": 2, "text": "APPLICATION IN REVIEW", "value": 228 },
+//       { "id": 3, "text": "DISBURSEMENT FIXED", "value": 0 },
+//       { "id": 4, "text": "LEADS IN PROCESS", "value": 9 },
+//       { "id": 5, "text": "ONLINE APPLICATIONS", "value": 1 },
+//       { "id": 6, "text": "PARTIAL DISBURSEMENT", "value": 96 },
+//       { "id": 7, "text": "PENDING QUERY", "value": 446 },
+//       { "id": 8, "text": "PRE RHDFC", "value": 0 },
+//       { "id": 9, "text": "PRE SANCTION", "value": 521 },
+//       { "id": 10, "text": "SANCTION", "value": 0 }
+//     ]
+//   }
+// }
+
 export class AppService {
-  getHello(): any {
-    return [
-      { "id": 1, "text": "ALL", "value": 9 },
-      { "id": 2, "text": "APPLICATION IN REVIEW", "value": 228 },
-      { "id": 3, "text": "DISBURSEMENT FIXED", "value": 0 },
-      { "id": 4, "text": "LEADS IN PROCESS", "value": 9 },
-      { "id": 5, "text": "ONLINE APPLICATIONS", "value": 1 },
-      { "id": 6, "text": "PARTIAL DISBURSEMENT", "value": 96 },
-      { "id": 7, "text": "PENDING QUERY", "value": 446 },
-      { "id": 8, "text": "PRE RHDFC", "value": 0 },
-      { "id": 9, "text": "PRE SANCTION", "value": 521 },
-      { "id": 10, "text": "SANCTION", "value": 0 }
-    ]
-  }
-}
+  private readonly data: DataItem[] = [
+    { id: 1, text: "ALL", value:14 },
+    { id: 2, text: "APPLICATION IN REVIEW", value: 228 },
+    { id: 3, text: "DISBURSEMENT FIXED", value: 0 },
+    { id: 4, text: "LEADS IN PROCESS", value: 9 },
+    { id: 5, text: "ONLINE APPLICATIONS", value: 1 },
+    { id: 6, text: "PARTIAL DISBURSEMENT", value: 96 },
+    { id: 7, text: "PENDING QUERY", value: 446 },
+    { id: 8, text: "PRE RHDFC", value: 0 },
+    { id: 9, text: "PRE SANCTION", value: 521 },
+    { id: 10, text: "SANCTION", value: 0 }
+  ];
+
+  getHello(): DataItem[] {
+    return this.data;
+  }}
