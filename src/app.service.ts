19f7394dcf4935ee31fad5c0882464b54f275a3b
mrinalrkumar <mrinal.rajeev.kumar@accenture.com>
2024-02-15 15:59:30 +0530

    Updated data

---
+++ src/app.service.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): any {
+    return [
+      { "id": 1, "text": "ALL", "value": 9 },
+      { "id": 2, "text": "APPLICATION IN REVIEW", "value": 228 },
+      { "id": 3, "text": "DISBURSEMENT FIXED", "value": 0 },
+      { "id": 4, "text": "LEADS IN PROCESS", "value": 9 },
+      { "id": 5, "text": "ONLINE APPLICATIONS", "value": 1 },
+      { "id": 6, "text": "PARTIAL DISBURSEMENT", "value": 96 },
+      { "id": 7, "text": "PENDING QUERY", "value": 446 },
+      { "id": 8, "text": "PRE RHDFC", "value": 0 },
+      { "id": 9, "text": "PRE SANCTION", "value": 521 },
+      { "id": 10, "text": "SANCTION", "value": 0 }
+    ]
+  }
+}
